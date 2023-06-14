5d36c12a0ce43886235927b5dbb12c8cbdefd7fc
milnershay <milnershay@gmail.com>
2023-06-14 22:18:06 +0300

    Co-authored-by: Yoad ben-david <yoadb@post.bgu.ac.il>

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,133 @@ router.get('/favorites', async (req,res,next) => {
 });
 
 
+/**
+ * This path returns the logged in user's last seen recipes
+ */
+router.get('getLastSeen', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const last = await user_utils.getLastViewed(user_id);
+    if (last.length === 0){
+      res.status(404).send("Last viewed not found")
+    } else{
+      res.status(200).send("returned last 3 seen recipes")
+    }
+  } catch(error){
+    next(error);
+  }
+})
 
+
+/**
+ * This path addes the viewed recipe to viewed
+ */
+router.post('addToSeen', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    await user_utils.addViewed(user_id, recipe_id)
+    res.status(200).send("added to viewed list")
+  } catch(error){
+    next(error);
+  }
+})
+
+
+
+/**
+ * This path adds a like to a recipe
+ */
+router.put('like', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipeId;
+    recipes = await DButils.execQuery("SELECT recipe_id from recipes");
+    if (recipes.find((x) => x.recipe_id !== recipe_id))
+      throw { status: 400, message: "Error, invalid recipe id" };
+    await user_utils.giveLike(recipe_id)
+    res.status(201).send("successfuly added like")
+  } catch(error){
+    next(error);
+  }
+})
+
+
+/**
+ * This path adds a new recipe from a logged in user
+ */
+router.post('/createRecipe', async (req,res,next) =>{
+  try{
+    const user_id = req.session.user_id;
+    let recipe_details = {
+      title: req.body.title,
+      ready_in_minutes: req.body.readyInMinutes,
+      vegetarian: req.body.vegetarian,
+      vegan: req.body.vegan,
+      gluten_free: req.body.gluten_free,
+      servings: req.body.servings,
+      instructions: req.body.instructions,
+      ingredients: req.body.ingredients,
+    }
+    await user_utils.addNewRecipe(user_id, recipe_details.title, recipe_details.ready_in_minutes, recipe_details.vegetarian, recipe_details.vegan, recipe_details.gluten_free, recipe_details.servings, recipe_details.instructions, recipe_details.ingredients, recipe_details.image)
+    res.status(201).send("New recipe has been added")
+  }catch(error){
+    next(error);
+  }
+}) 
+
+
+/**
+ * This path adds a new family recipe from a logged in user
+ */
+// add new recipe to family recipes
+
+router.post('/FamilyRecipe', async (req,res,next) =>{
+  try{
+    const user_id = req.session.user_id;
+    let recipe_details = {
+      title: req.body.title,
+      author: req.body.author,
+      whenDoWeEat: req.body.whenDoWeEat,
+      instructions: req.body.instructions,
+      ingredients: req.body.ingredients,
+      image: req.body.image,
+    }
+    await user_utils.addNewFamilyRecipe(user_id, recipe_details.title, recipe_details.author, recipe_details.whenDoWeEat, recipe_details.ingredients, recipe_details.instructions, recipe_details.image)
+    res.status(201).send("New recipe has been added")
+  }catch(error){
+    next(error);
+  }
+}) 
+
+
+/**
+ * This path gets a logged users personal recipes
+ */
+
+router.get('/personalRecipe', async (req,res,next) =>{
+  try{
+    const user_id = req.session.user_id;
+    await user_utils.getUserCreated(user_id)
+    res.status(200).send("data about the user's created recipes recieved")
+  }catch(error){
+    next(error);
+  }
+}) 
+
+
+/**
+ * This path gets a logged users personal recipes
+ */
+
+router.get('/FamilyRecipe', async (req,res,next) =>{
+  try{
+    const user_id = req.session.user_id;
+    await user_utils.getFamily(user_id)
+    res.status(200).send("data about the user's created recipes recieved")
+  }catch(error){
+    next(error);
+  }
+}) 
 
 module.exports = router;
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
+const DButils = require("./DButils");
 
 
 
@@ -22,6 +23,12 @@ async function getRecipeInformation(recipe_id) {
 
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
+
+    if (recipe_info === null){
+        recipe_info = await DButils.execQuery(`select recipe_id,user_id,title,readyInMinutes,image,popularity,vegan,vegetarian,glutenFree from recipes where recipe_id='${recipe_id}'`)
+        return recipe_info;
+    }
+
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
 
     return {
@@ -37,8 +44,64 @@ async function getRecipeDetails(recipe_id) {
     }
 }
 
+async function getRecipeFullDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+
+    if (recipe_info === null){
+        recipe_info = await DButils.execQuery(`select * from recipes where recipe_id='${recipe_id}'`)
+
+    }
+    return recipe_info;
+}
 
 
+async function getRandoms(){
+    let randoms = await axios.get(`${api_domain}/random?number=3`, {
+        params: {
+            apiKey: process.env.spooncular_apiKey
+        }
+    })
+    let recipes = []
+    for (let recipe of randoms){
+        let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe.data;
+
+        recipes.push( {
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,
+            
+        })
+    }
+    return recipes;
+}
+
+async function search(queried){
+    let results = await axios.get(`${api_domain}/complexSearch`, {
+        params: {
+            apiKey: process.env.spooncular_apiKey,
+            query: queried.query,
+            cuisine: queried.cuisine,
+            diet: queried.diet,
+            intolerances: queried.intolerances,
+            number: queried.number,
+            instructionsRequired: true,
+        }
+    })
+
+    return results;
+}
+
+
+
+
+exports.search = search;
+exports.getRandoms = getRandoms;
+exports.getRecipeFullDetails = getRecipeFullDetails;
 exports.getRecipeDetails = getRecipeDetails;
 
 
